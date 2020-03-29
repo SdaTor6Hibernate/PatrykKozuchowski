be112b21a3f87e84d9a4625ecb916e9ab9437cd3
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-03-29 10:16:30 +0200

    3

--- src/main/java/przyklad1.java
+++ src/main/java/przyklad1.java
@@ -1,12 +1,14 @@
-import com.mysql.cj.Session;
+
+import model.Address;
 import model.Country;
+import org.hibernate.Session;
 import util.HibernateUtil;
 
 public class przyklad1 {
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        Country country = session.find(Country.class, 1);
+        Address address= session.find(Address.class, 3);
         session.close();
-        System.out.println(country.toString());
+        System.out.println(address.toString());
     }
 }
